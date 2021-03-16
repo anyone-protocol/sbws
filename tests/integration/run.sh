7d26503cb32668c107bef3bae8389170c8ded14b
juga0 <juga@riseup.net>
2021-03-16 14:28:09 +0000

    fix: Move to declarative setup.cfg
    Also:
    - Update versioneer
    - And include other source distribution files in MANIFEST.in
    - Add project URLs
    - Add formatter and linter dependencies and configurations.
    - tox: Remove travis, fix python environments
    - tox: Remove extra coverage options and add them in .coveragerc.

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -12,6 +12,7 @@ wget -O/dev/null http://127.0.0.1:28888/sbws.bin
 sbws -c tests/integration/sbws_testnet.ini scanner
 sbws -c tests/integration/sbws_testnet.ini generate
 # Run integration tests
-coverage run -a --rcfile=.coveragerc --source=sbws -m pytest -s tests/integration -vv
+python -m coverage run --append --module pytest -svv tests/integration
+
 sbws -c tests/integration/sbws_testnet.ini cleanup
 tests/integration/stop_chutney.sh
