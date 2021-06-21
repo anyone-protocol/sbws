685d3ea6b5ccbb96bae6fc7ca5ebae7509456f20
juga0 <juga@riseup.net>
2021-06-21 10:08:06 +0000

    fix: tests: Consensus bandwidth might not be 0
    Since tor version 0.4.7.0-alpha-dev with #40337 patch, chutney relays
    notice bandwidth changes.

--- tests/integration/lib/test_relaylist.py
+++ tests/integration/lib/test_relaylist.py
@@ -5,7 +5,8 @@ def test_relay_properties(rl):
     assert "Authority" in relay.flags
     assert not relay.exit_policy or not relay.exit_policy.is_exiting_allowed()
     assert relay.average_bandwidth == 1073741824
-    assert relay.consensus_bandwidth == 0
+    # Since tor version 0.4.7.0-alpha-dev, #40337 patch, chutney relays notice
+    # bandwidth changes, so consensus bandwidth might be higher than 0.
     assert relay.address == "127.0.0.1"
 
 
