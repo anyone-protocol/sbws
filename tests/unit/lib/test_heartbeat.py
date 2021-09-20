32652d70bbd68bf33780681ba965821508324bf9
Milad-Laly <68471307+Milad-Laly@users.noreply.github.com>
2021-09-20 20:46:15 +0100

    improvements and being inline with pep8
    As juga suggested in the commits, I've done.
    I tried to figure out another way instead of manually defining the value but couldn't figure it out

--- tests/unit/lib/test_heartbeat.py
+++ tests/unit/lib/test_heartbeat.py
@@ -4,21 +4,22 @@ import logging
 from sbws.lib import heartbeat
 
 
-def test_register_measured_fpr(conf, caplog):
+def test_register_measured_fpr(conf):
     """
     Test that checks if the values entered into the function
     register_measured_fpr is inputted into the variable measured_fp.set
 
     Asserts if the value of measured_fp_set is the same value
-    inputted into the function register_measured_fpr which is "123"
+    inputted into the function register_measured_fpr which is "A"
     """
     hbeat = heartbeat.Heartbeat(conf.getpath("paths", "state_fname"))
-    hbeat.register_measured_fpr("123")
-    assert hbeat.measured_fp_set == {"123"}
-    caplog.clear()
+    hbeat.register_measured_fpr("A")
+    hbeat.register_measured_fpr("B")
+    assert "A" in hbeat.measured_fp_set
+    assert "B" in hbeat.measured_fp_set
 
 
-def test_register_consensus_fprs(conf, caplog):
+def test_register_consensus_fprs(conf):
     """
     Test that checks if the values entered into the function
     register_consensus_fprs is inputted into the variable consensus_fp_set
@@ -29,7 +30,6 @@ def test_register_consensus_fprs(conf, caplog):
     hbeat = heartbeat.Heartbeat(conf.getpath("paths", "state_fname"))
     hbeat.register_consensus_fprs(["4", "5", "6"])
     assert hbeat.consensus_fp_set == {"4", "5", "6"}
-    caplog.clear()
 
 
 def test_print_heartbeat_message(conf, caplog):
@@ -42,24 +42,21 @@ def test_print_heartbeat_message(conf, caplog):
     hbeat = heartbeat.Heartbeat(conf.getpath("paths", "state_fname"))
     hbeat.register_measured_fpr("12")
     hbeat.register_consensus_fprs(["1", "2", "4"])
-
-    caplog.clear()
     hbeat.print_heartbeat_message()
-    caplog.set_level(logging.INFO)
 
     assert hbeat.previous_measurement_percent == 33
     assert "Run None main loops." in caplog.records[0].getMessage()
     assert "Measured in total 1 (33%)" in caplog.records[1].getMessage()
-    message_Relay_not_measured = "3 relays still not measured"
-    assert message_Relay_not_measured in caplog.records[2].getMessage()
+    log_relay_not_measured = "3 relays still not measured"
+    assert log_relay_not_measured in caplog.records[2].getMessage()
 
     caplog.clear()
-    new_Percent = round(
+    new_percent = round(
         len(hbeat.measured_fp_set) / len(hbeat.consensus_fp_set) * 100
     )
-    hbeat.previous_measurement_percent = new_Percent + 100
+    hbeat.previous_measurement_percent = new_percent + 100
     caplog.set_level(logging.WARNING)
 
     hbeat.print_heartbeat_message()
-    message_No_Progress = "There is no progress measuring new unique relays."
-    assert message_No_Progress in caplog.records[0].getMessage()
+    log_no_progress = "There is no progress measuring new unique relays."
+    assert log_no_progress in caplog.records[0].getMessage()
