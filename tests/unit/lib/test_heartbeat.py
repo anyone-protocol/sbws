c861ccc52df9d81b322fcaf608eadd675f1c2115
Milad-Laly <68471307+Milad-Laly@users.noreply.github.com>
2021-09-20 03:12:55 +0100

    heartbeat coverage improvement
    This should increase the coverage to 100% and should be passing the tests/commands when running tox

--- tests/unit/lib/test_heartbeat.py
+++ tests/unit/lib/test_heartbeat.py
@@ -1,34 +1,65 @@
 """Unit tests for heartbeat"""
 import logging
 
-import pytest
-
 from sbws.lib import heartbeat
-from sbws.util.state import State
 
 
-@pytest.mark.skip(reason="increment_recent_measurement_attempt() disabled")
-def test_total_measured_percent(conf, caplog):
-    state = State(conf["paths"]["state_fname"])
-    state["recent_priority_list"] = [1, 2, 3]
+def test_register_measured_fpr(conf, caplog):
+    """
+    Test that checks if the values entered into the function
+    register_measured_fpr is inputted into the variable measured_fp.set
+
+    Asserts if the value of measured_fp_set is the same value
+    inputted into the function register_measured_fpr which is "123"
+    """
     hbeat = heartbeat.Heartbeat(conf.getpath("paths", "state_fname"))
+    hbeat.register_measured_fpr("123")
+    assert hbeat.measured_fp_set == {"123"}
+    caplog.clear()
 
-    hbeat.register_consensus_fprs(["A", "B", "C"])
 
-    hbeat.register_measured_fpr("A")
-    hbeat.register_measured_fpr("B")
+def test_register_consensus_fprs(conf, caplog):
+    """
+    Test that checks if the values entered into the function
+    register_consensus_fprs is inputted into the variable consensus_fp_set
 
+    Asserts if the value of consensus_fp_set is the same value
+    inputted into the function register_consensus_fprs which is ["4","5","6"]
+    """
+    hbeat = heartbeat.Heartbeat(conf.getpath("paths", "state_fname"))
+    hbeat.register_consensus_fprs(["4", "5", "6"])
+    assert hbeat.consensus_fp_set == {"4", "5", "6"}
+    caplog.clear()
+
+
+def test_print_heartbeat_message(conf, caplog):
+    """
+    Test that checks if the print_message function is working as predicted.
+    Also, to check if the text is also logged as expected.
+
+    Asserts that the specified text is present in the log
+    """
+    hbeat = heartbeat.Heartbeat(conf.getpath("paths", "state_fname"))
+    hbeat.register_measured_fpr("12")
+    hbeat.register_consensus_fprs(["1", "2", "4"])
+
+    caplog.clear()
+    hbeat.print_heartbeat_message()
     caplog.set_level(logging.INFO)
 
-    assert hbeat.previous_measurement_percent == 0
+    assert hbeat.previous_measurement_percent == 33
+    assert "Run None main loops." in caplog.records[0].getMessage()
+    assert "Measured in total 1 (33%)" in caplog.records[1].getMessage()
+    message2 = "3 relays still not measured"
+    assert message2 in caplog.records[2].getMessage()
+
+    caplog.clear()
+    newPercent = round(
+        len(hbeat.measured_fp_set) / len(hbeat.consensus_fp_set) * 100
+    )
+    hbeat.previous_measurement_percent = newPercent + 100
+    caplog.set_level(logging.WARNING)
 
     hbeat.print_heartbeat_message()
-
-    assert hbeat.previous_measurement_percent == 67
-    assert 0 == caplog.records[0].getMessage().find("Run 3 main loops.")
-    assert 0 == caplog.records[1].getMessage().find(
-        "Measured in total 2 (67%)"
-    )
-    assert 0 == caplog.records[2].getMessage().find(
-        "1 relays still not measured"
-    )
+    messageWarning = "There is no progress measuring new unique relays."
+    assert messageWarning in caplog.records[0].getMessage()
